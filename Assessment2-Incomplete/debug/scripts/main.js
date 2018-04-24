ac8dd987b6c0664a5c58d2e69826e12a25a8d6be
Pamela Martin <pamelam@uark.edu>
2018-04-24 11:54:07 -0500

    debugger and html forms update

--- Assessment2-Incomplete/debug/scripts/main.js
+++ Assessment2-Incomplete/debug/scripts/main.js
@@ -1,4 +1,3 @@
-
 /*
     Read the comments and follow the instructions
     Total Points available -> 80
@@ -139,9 +138,6 @@ console.log('-----------------------------');
 
 
 
-
-
-
 /*
     Write a JavaScript function that computes the sum
     all of the numbers in an array.
@@ -149,20 +145,22 @@ console.log('-----------------------------');
     var foo = sumArray([2,5,6]);
     console.log(foo) // Outputs 13
 */
-/*
 
-function sumArray(x){
-  var array=[];
-  array.push(x);
-  for(i=0; i<arguements.length; i++){
-    array +=arguements[i];
-  }
-}
-console.log()
+function sumArray(y){
+           var total =  0;
+            for(var i=0;i<y.length;i++)
+              {                  
+                if(isNaN(y[i])){
+                continue;
+                 }
+                  total += Number(y[i]);
+               }
+             return total;
+            }
 
-*/
 
-/*
+
+
 console.log('-------------  SUM ARRAY ----------------'); 
 foo = sumArray([2, 5, 6]);
 console.log(foo); // Outputs 13
@@ -171,7 +169,7 @@ foo = sumArray([2, 5, 6, 2, 5, 6]);
 console.log(foo); // Outputs 26
 
 console.log('-----------------------------');
-*/
+
 
 
 /*
@@ -212,23 +210,49 @@ console.log(foo2) //Outputs "50%"
     var newD = addDays(d, 10);
     console.log(newD);  //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard Time)
 */
+      
+console.log('------------ Add Days-----------------');
 
+
+
+
+function addDays(a, b){
+ var newDate =a.setDate(a.getDate() + b);
+    var day = a.getDay();
+    var date = a.getDate()+b;
+    var month = a.getMonth();
+    var year = a.getFullYear();
+    var hour = a.getHours();
+    var min=a.getMinutes();
+    var sec= a.getSeconds();
+    //var newDate = day+ " " +month+ ' ' +date + ' ' +year+' '+hour+ ' '+min+' '+sec;
+    return newDate.toUTCString();
+   
+}
 /*
-Write addDays function here
+
+function addDays(a, b){
+    var newDate = {
+        a.getDay(),
+        a.getMonth(),
+        (a.getDate()+b),
+        a.getFullYear(),
+        a.getHours(),
+        a.getMinutes(),
+        a.getSeconds();  
+    } 
+    return new Date(newDate);
+  }
 */
-
-
-/* Uncomment this to test addDays function
 var d = new Date('1/1/2001');
 var newD = addDays(d, 10);
 console.log(newD); //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard Time)
 d = new Date('9/10/2001');
-newD = addDays(d, 1000);
+newD = addDays(d, 100);
 console.log(newD); //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard Time)
-*/
-
-
+console.log('-----------------------------');
 /*
+
     Challenge - 
     Write a javascript function to take an array of numbers
     and sorts them in ascending order.
@@ -236,15 +260,15 @@ console.log(newD); //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard
     var array1 = [3, 5, 1, 10];
     console.log(sortArray(array1); //outputs [1,3,5,10]
 */
+function sortArray(x) {
+   return x.sort(function(a, b){return a-b});
+    
+}
+console.log('------------ Bonus Challenge-----------------');
 
-/*
-Write sortArray function here
-*/
 
-/* Uncomment this to test sortArray function
 var array1 = [3, 5, 1, 10];
 console.log(sortArray(array1)); //outputs [1,3,5,10]
-*/
 
 
 
